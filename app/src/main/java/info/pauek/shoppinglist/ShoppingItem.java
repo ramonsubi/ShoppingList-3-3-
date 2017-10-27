47ac424d56f9cf9cb297701f6c02690c1ad6aead
ramonsubi <ramonsubiratsmorral@gmail.com>
2017-10-27 20:22:11 +0200

    Practica 8: ShoppingList

---
+++ app/src/main/java/info/pauek/shoppinglist/ShoppingItem.java
@@ -0,0 +1,33 @@
+package info.pauek.shoppinglist;
+
+
+public class ShoppingItem {
+    private String text;
+    private boolean checked;
+
+    public ShoppingItem(String text) {
+        this.text = text;
+        this.checked = false;
+    }
+
+    public ShoppingItem(String text, boolean checked) {
+        this.text = text;
+        this.checked = checked;
+    }
+
+    public String getText() {
+        return text;
+    }
+    public void setText(String text) {
+        this.text = text;
+    }
+    public boolean isChecked() {
+        return checked;
+    }
+    public void setChecked(boolean checked) {
+        this.checked = checked;
+    }
+    public void toggleChecked() {
+        this.checked = !this.checked;
+    }
+}
